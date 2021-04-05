9de8128edfb913b4243b7f9aca1e4ee4e9db9484
Noemi Krysiak-Wolanska <noemi.krysiak@gmail.com>
2021-04-05 19:35:43 +0200

    add columns and rows to the Table And add ComboBoxColumn

--- PdfViewer/PdfApp.cs
+++ PdfViewer/PdfApp.cs
@@ -36,23 +36,43 @@ namespace PdfViewer
             var cvRepository = new CvRepository();
             var cvs = cvRepository.GetCvDtos(directoryPath);
             dataGridView1.DataSource = cvs;
+            
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Id", typeof(int));
+            dataTable.Columns.Add("FileName", typeof(string));
+            dataTable.Columns.Add("FilePath", typeof(string));
+            dataTable.Columns.Add("Status", typeof(Status));
+
+            foreach (CvDto cvDto in cvs)
+            {
+                dataTable.Rows.Add(cvDto.Id, cvDto.FileName,cvDto.FilePath,cvDto.Status);
+            }
+            dataGridView1.DataSource = dataTable;
+
+            DataGridViewComboBoxColumn dcomboStatus = new DataGridViewComboBoxColumn();
+            dcomboStatus.HeaderText = "Status";
+            foreach(Status status in Enum.GetValues(typeof(Status)))
+            {
+                dcomboStatus.Items.Add(status);
+            }
+            dataGridView1.Columns.Add(dcomboStatus);
+
+
+
         }
 
-/*        public void showPdf(string filePath)
-        {
-            byte[] bytes = System.IO.File.ReadAllBytes(filepath);
-            var stream = new MemoryStream(bytes);
-            PdfDocument pdfDocumnet = PdfDocument.Load(stream);
-            pdf.Document = pdfDocumnet;
-        }*/
+        /*        public void showPdf(string filePath)
+                {
+                    byte[] bytes = System.IO.File.ReadAllBytes(filepath);
+                    var stream = new MemoryStream(bytes);
+                    PdfDocument pdfDocumnet = PdfDocument.Load(stream);
+                    pdf.Document = pdfDocumnet;
+                }*/
 
         private void PdfApp_Load(object sender, EventArgs e)
         {
         }
 
-        private void dataGridView1_CancelRowEdit(object sender, QuestionEventArgs e)
-        {
-
-        }
+       
     }
 }
